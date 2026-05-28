edf922270b97b568babba1d724d13e02e2b1e4a7
Александр <krupkocaca@gmail.com>
2026-05-29 03:50:39 +0700

    Добавлены базовые css стили

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "expense-list"
 LOGOUT_REDIRECT_URL = "login"
