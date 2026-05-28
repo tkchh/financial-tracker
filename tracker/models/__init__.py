7e6fdb84ccff03a007d2c4bd6728ff20712c45a4
Александр <krupkocaca@gmail.com>
2026-05-29 02:08:10 +0700

    Созданы модели категорий расходов и доходов

---
+++ tracker/models/__init__.py
@@ -0,0 +1,9 @@
+
+
+__all__ = (
+    "IncomeCategory",
+    "ExpenseCategory",
+)
+
+from tracker.models.expense_category import ExpenseCategory
+from tracker.models.income_category import IncomeCategory
---
+++ tracker/models/expense_category.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class ExpenseCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='expense_categories')
+    name = models.CharField(max_length=100)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name
